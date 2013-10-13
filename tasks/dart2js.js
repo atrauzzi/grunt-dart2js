631f48699e4be041c9a4bf0a345c6f42603c8683
Alexander Trauzzi <atrauzzi@gmail.com>
2013-10-13 09:42:39 -0500

    Cleaning, beep beep beep.

--- tasks/dart2js.js
+++ tasks/dart2js.js
@@ -60,35 +60,3 @@ module.exports = function(grunt) {
 	});
 
 };
-
-//
-//
-//
-
-/*
-// Iterate over all specified file groups.
-this.files.forEach(function(f) {
-// Concat specified files.
-var src = f.src.filter(function(filepath) {
-// Warn on and remove invalid source files (if nonull was set).
-if (!grunt.file.exists(filepath)) {
-grunt.log.warn('Source file "' + filepath + '" not found.');
-return false;
-} else {
-return true;
-}
-}).map(function(filepath) {
-// Read file source.
-return grunt.file.read(filepath);
-}).join(grunt.util.normalizelf(options.separator));
-
-// Handle options.
-src += options.punctuation;
-
-// Write the destination file.
-grunt.file.write(f.dest, src);
-
-// Print a success message.
-grunt.log.writeln('File "' + f.dest + '" created.');
-});
-*/

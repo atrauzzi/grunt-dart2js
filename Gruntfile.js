cb3e10c53ea184dcae540d73cc84eb9ad311c390
Alexander Trauzzi <atrauzzi@gmail.com>
2013-10-13 09:38:07 -0500

    First crack at what I think is the complete setup?

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,73 +1,32 @@
 /*
- * grunt-dart2js
- * https://github.com/atrauzzi/grunt-dart2js
- *
- * Copyright (c) 2013 Alexander Trauzzi
- * Licensed under the MIT license.
- */
+* grunt-dart2js
+* https://github.com/atrauzzi/grunt-dart2js
+*
+* Copyright (c) 2013 Alexander Trauzzi
+* Licensed under the MIT license.
+*/
 
 'use strict';
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    jshint: {
-      all: [
-        'Gruntfile.js',
-        'tasks/*.js',
-        '<%= nodeunit.tests %>',
-      ],
-      options: {
-        jshintrc: '.jshintrc',
-      },
-    },
+	// Project configuration.
+	grunt.initConfig({
 
-    // Before generating any new files, remove any previously-created files.
-    clean: {
-      tests: ['tmp'],
-    },
+		// Configuration to be run (and then tested).
+		dart2js: {
+			compile: {
+				options: {
+				},
+				files: {
+					"public/main.js": "app/dart/main.dart",
+				}
+			}
+		},
 
-    // Configuration to be run (and then tested).
-    dart2js: {
-      default_options: {
-        options: {
-        },
-        files: {
-          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123'],
-        },
-      },
-      custom_options: {
-        options: {
-          separator: ': ',
-          punctuation: ' !!!',
-        },
-        files: {
-          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123'],
-        },
-      },
-    },
+	});
 
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js'],
-    },
-
-  });
-
-  // Actually load this plugin's task(s).
-  grunt.loadTasks('tasks');
-
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'dart2js', 'nodeunit']);
-
-  // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test']);
+	// Actually load this plugin's task(s).
+	grunt.loadTasks('tasks');
 
 };
--- tasks/dart2js.js
+++ tasks/dart2js.js
@@ -1,50 +1,94 @@
 /*
- * grunt-dart2js
- * https://github.com/atrauzzi/grunt-dart2js
- *
- * Copyright (c) 2013 Alexander Trauzzi
- * Licensed under the MIT license.
- */
+* grunt-dart2js
+* https://github.com/atrauzzi/grunt-dart2js
+*
+* Copyright (c) 2013 Alexander Trauzzi
+* Licensed under the MIT license.
+*/
 
 'use strict';
 
 module.exports = function(grunt) {
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+	var numCPUs = require('os').cpus().length;
 
-  grunt.registerMultiTask('dart2js', 'Compile Dart to JavaScript.', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
-    });
+	grunt.registerMultiTask('dart2js', 'Compile Dart to JavaScript.', function() {
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
+		// Merge task-specific and/or target-specific options with these defaults.
+		var options = this.options({
+			// If someone just quickly grabbed the Dart SDK, it's likely to be here.
+			"dart2js_bin": "~/dart/dart-sdk/bin/dart2js",
+			"minify": false
+		});
 
-      // Handle options.
-      src += options.punctuation;
+		// For every target & source, async to the number of CPUs.
+		grunt.util.async.forEachLimit(this.files, numCPUs, function (file, next) {
 
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
+			var src = file.src[0];
 
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
-    });
-  });
+			if(!grunt.file.exists(src)) {
+				grunt.log.warn('Source file "' + src + '" not found.');
+				return next();
+			}
+
+			var args = [
+				"--out=" + file.dest,
+				src
+			];
+
+			// If minification is desired (probably not).
+			if(options.minify) {
+				args.push("--minify");
+			}
+
+			var process = grunt.util.spawn(
+				{
+					cmd: options.dart2js_bin,
+					args: args,
+					opts: {
+						stdio: 'inherit'
+					}
+				},
+				function (error, result, code) {
+					grunt.log.writeln("Error code:" + error);
+					next(error);
+				}
+			);
+
+		});
+
+	});
 
 };
+
+//
+//
+//
+
+/*
+// Iterate over all specified file groups.
+this.files.forEach(function(f) {
+// Concat specified files.
+var src = f.src.filter(function(filepath) {
+// Warn on and remove invalid source files (if nonull was set).
+if (!grunt.file.exists(filepath)) {
+grunt.log.warn('Source file "' + filepath + '" not found.');
+return false;
+} else {
+return true;
+}
+}).map(function(filepath) {
+// Read file source.
+return grunt.file.read(filepath);
+}).join(grunt.util.normalizelf(options.separator));
+
+// Handle options.
+src += options.punctuation;
+
+// Write the destination file.
+grunt.file.write(f.dest, src);
+
+// Print a success message.
+grunt.log.writeln('File "' + f.dest + '" created.');
+});
+*/
